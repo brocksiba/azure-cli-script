35512b3a9781d1995373f854952c1ea873e054be
brocksiba <129262234+brocksiba@users.noreply.github.com>
2026-08-05 12:20:53 +0530

    Update create-rg.sh

--- 01-resource-group/create-rg.sh
+++ 01-resource-group/create-rg.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-# Configuration Variables
-RESOURCE_GROUP="my-demo-rg"
-LOCATION="eastus"
-
 # Resource Group Creation
-az group create \
-  --name $RESOURCE_GROUP \
-  --location $LOCATION
+az group create --name MyResourceGroup --location centralindia
+
